ce7ce0b384309260cfde69b54bd966e8482cdd7f
Affansaleem <m.affansalim@gmail.com>
2024-09-12 00:51:03 +0500

    Initial Commit - IOS, Android, Web setup with firebase Successful

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyA5DdtzSdlp3AKi_MwnasdLKAYqeXTvS7c',
+            appId: '1:57276007591:web:6889e170e709a4ff6c4782',
+            messagingSenderId: '57276007591',
+            projectId: 'instagram-1b4b0',
+            storageBucket: 'instagram-1b4b0.appspot.com'));
+  } else {
+     await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: const ResponsiveLayout(
+            webScreenLayout: WebScreenLayout(),
+            mobileScreenLayout: MobileScreenLayout()));
+  }
+}
